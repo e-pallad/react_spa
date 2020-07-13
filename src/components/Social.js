f6765383a58f673cfb41f793ca0a5a254fda4302
Eric Pallad <er10092@mailingwork.local>
2020-07-13 08:28:08 +0200

    changed sm-links

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,12 +4,12 @@ class Social extends Component {
     render() {
         return (
             <div class="social">
-                <a href="https://codepen.io/naafi" target="_blank"><i class="fab fa-codepen"></i></a>
-                <a href="https://github.com/naaficodes" target="_blank"><i class="fab fa-github"></i></a>
-                <a href="https://Instagram.com/iam_naafi" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="https://www.facebook.com/abdul.w.naafi" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                <a href="https://Linkedin.com/in/naafi" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                <a href="https://medium.com/@iam_naafi" target="_blank"><i class="fab fa-medium-m"></i></a>
+                <a href="https://codepen.io/e-pallad" target="_blank"><i class="fab fa-codepen"></i></a>
+                <a href="https://github.com/e-pallad" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://Instagram.com/trvl_sct" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://Linkedin.com/in/eric-pallad" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://medium.com/" target="_blank"><i class="fab fa-medium-m"></i></a>
             </div>
         )       
     }
